d227dc6af511fe18abb7a47f6ac8ebed9c210abf
kyungchan <kkc8983@gmail.com>
2020-02-02 23:15:14 +0900

    Create vue project by Vue CLI 3 + Vuetify, i18n

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  transpileDependencies: ["vuetify"],
+
+  pluginOptions: {
+    i18n: {
+      locale: "vn",
+      fallbackLocale: "ko",
+      localeDir: "locales",
+      enableInSFC: false
+    }
+  }
+};
